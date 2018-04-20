33a247ba7f354c203083c2ed563fb7e1d7a68281
Juanfra Aldasoro <juan@nicethemes.com>
2018-04-20 16:15:38 -0300

    Refactor the extension according to the extension template.

--- tribe-ext-advance-mini-calendar.php
+++ tribe-ext-advance-mini-calendar.php
@@ -1,11 +1,13 @@
 <?php
 /**
- * Plugin name: The Events Calendar: Advance Mini Calendar Widget
- * Description: Tries to force the minicalendar widget to show the month of the next upcoming event by default, rather than simply showing the current month (which might be empty).
- * Author:      Modern Tribe, Inc
- * Author URI:  http://theeventscalendar.com
- * Version:     1.0.0
- * License:     GPL v3 - see http://www.gnu.org/licenses/gpl.html
+ * Plugin name:       The Events Calendar: Advance Mini Calendar Widget
+ * Description:       Tries to force the minicalendar widget to show the month of the next upcoming event by default, rather than simply showing the current month (which might be empty).
+ * Version:           1.0.0
+ * Extension Class:   Tribe__Extension__Advance_Minical
+ * GitHub Plugin URI: https://github.com/mt-support/tribe-ext-advance-mini-calendar/
+ * Author:            Modern Tribe, Inc
+ * Author URI:        http://theeventscalendar.com
+ * License:           GPL v3 - see http://www.gnu.org/licenses/gpl.html
  *
  * The Events Calendar: Advance Mini Calendar Widget
  * Copyright (C) 2018 Modern Tribe, Inc
@@ -24,99 +26,152 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-class Tribe_Advance_Minical {
-
-	protected $target_date = false;
-
+// Do not load unless Tribe Common is fully loaded and our class does not yet exist.
+if (
+	class_exists( 'Tribe__Extension' )
+	&& ! class_exists( 'Tribe__Extension__Advance_Minical' )
+) {
 	/**
-	 * Sets up auto advance for minicalendar widgets. If an optional target date is provided that will be used to
-	 * set the month.
-	 *
-	 * @param bool $target_date
+	 * Extension main class, class begins loading on init() function.
 	 */
-	public function __construct( $target_date = false ) {
+	class Tribe__Extension__Advance_Minical extends Tribe__Extension {
 
-		if ( is_admin() ) {
-			return;
+		protected $target_date = false;
+
+		/**
+		 * Set the minimum required version of The Events Calendar
+		 * and The Events Calendar Pro
+		 */
+		public function construct() {
+
+			// Add required plugins
+			$this->add_required_plugin( 'Tribe__Events__Main', '4.3.3' );
+			$this->add_required_plugin( 'Tribe__Events__Pro__Main', '4.3.3' );
+			$this->set_version( '1.0.0' );
+
 		}
 
-		$this->target_date = $target_date;
+		/**
+		 * Extension initialization and hooks.
+		 */
+		public function init( $target_date = false ) {
 
-		add_action( 'wp_loaded', array( $this, 'set_target_date' ) );
-		add_filter( 'widget_display_callback', array( $this, 'advance_minical' ), 20, 2 );
-	}
+			if ( is_admin() ) {
+				return;
+			}
 
-	/**
-	 * Basic check to help filter out spurious date formats or automatically determine
-	 * the next most appropriate date to use.
-	 */
-	public function set_target_date() {
+			$this->target_date = $target_date;
 
-		if ( ! is_string($this->target_date) || 1 !== preg_match( '#^\d{4}-\d{2}(-\d{2})?$# ', $this->target_date ) ) {
-			$this->target_date = $this->next_upcoming_date();
+			add_action( 'wp_loaded', array( $this, 'set_target_date' ) );
+			add_filter( 'widget_display_callback', array( $this, 'advance_minical' ), 20, 2 );
+
 		}
-	}
 
-	public function advance_minical( $instance, $widget ) {
+		/**
+		 * Basic check to help filter out spurious date formats or automatically determine
+		 * the next most appropriate date to use.
+		 *
+		 * @since 1.0.0
+		 */
+		public function set_target_date() {
 
-		if ( 'tribe-mini-calendar' !== $widget->id_base || isset( $instance['eventDate'] ) ) {
+			if ( ! is_string( $this->target_date ) || 1 !== preg_match( '#^\d{4}-\d{2}(-\d{2})?$# ', $this->target_date ) ) {
+				$this->target_date = $this->next_upcoming_date();
+			}
+		}
+
+		/**
+		 * Check if the functionality should be applied, given the
+		 * context and the instance
+		 *
+		 * @since 1.0.0
+		 *
+		 * @param array     $instance  The current widget instance's settings.
+		 * @param WP_Widget $widget    The current widget instance.
+		 *
+		 * @return array $instance
+		 */
+		public function advance_minical( $instance, $widget ) {
+
+			if ( 'tribe-mini-calendar' !== $widget->id_base || isset( $instance['eventDate'] ) ) {
+				return $instance;
+			}
+
+			if ( date( 'Y-m' ) === $this->target_date ) {
+				return $instance;
+			}
+
+			add_action( 'tribe_before_get_template_part', array( $this, 'modify_list_query' ), 5 );
+
+			$instance['eventDate'] = $this->target_date;
+
 			return $instance;
 		}
 
-		if ( date( 'Y-m' ) === $this->target_date ) {
-			return $instance;
+		/**
+		 * Check if the query amendments should be triggered or not.
+		 * (if the template is the Mini Calendar List Loop)
+		 *
+		 * @since 1.0.0
+		 *
+		 * @param string $template
+		 *
+		 */
+		public function modify_list_query( $template ) {
+
+			if ( false === strpos( $template, 'mini-calendar/list.php' ) ) {
+				return;
+			}
+
+			add_action( 'parse_query', array( $this, 'amend_list_query' ) );
 		}
 
-		add_action( 'tribe_before_get_template_part', array( $this, 'modify_list_query' ), 5 );
+		/**
+		 * Modify the Mini Calendar List Loop query
+		 *
+		 * @since 1.0.0
+		 *
+		 * @param WP_Query $query
+		 */
+		public function amend_list_query( $query ) {
 
-		$instance['eventDate'] = $this->target_date;
+			// Run this once only.
+			remove_action( 'parse_query', array( $this, 'amend_list_query' ) );
 
-		return $instance;
-	}
+			$the_query = $query->query_vars;
 
-	public function modify_list_query( $template ) {
+			$the_query['start_date'] = $this->target_date . '-01';
+			$last_day = Tribe__Date_Utils::get_last_day_of_month( strtotime( $the_query['start_date'] ) );
+			$the_query['end_date'] = substr_replace( $the_query['start_date'], $last_day, -2 );
+			$the_query['end_date'] = tribe_end_of_day( $the_query['end_date'] );
 
-		if ( false === strpos( $template, 'mini-calendar/list.php' ) ) {
-			return;
+			$query->query_vars = $the_query;
 		}
 
-		add_action( 'parse_query', array( $this, 'amend_list_query' ) );
-	}
+		/**
+		 * Get the next upcoming event date
+		 *
+		 * @since 1.0.0
+		 *
+		 * @return string $start_date
+		 */
+		protected function next_upcoming_date() {
+			$next_event = tribe_get_events( array(
+				'eventDisplay'   => 'list',
+				'posts_per_page' => 1,
+				'start_date'     => date( Tribe__Date_Utils::DBDATEFORMAT )
+			));
 
-	public function amend_list_query( $query ) {
+			$start_date = date( 'Y-m' );
 
-		// Run this once only.
-		remove_action( 'parse_query', array( $this, 'amend_list_query' ) );
+			if ( ! empty( $next_event ) || isset( $next_event[0] ) ) {
+				$next_event_date = tribe_get_start_date( $next_event[0]->ID, false, 'Y-m' );
 
-		$the_query = $query->query_vars;
+				// Prevent calendar from rewinding to the start of a currently ongoing event
+				$start_date = ( $next_event_date > $start_date ) ? $next_event_date : $start_date;
+			}
 
-		$the_query['start_date'] = $this->target_date . '-01';
-		$last_day = Tribe__Date_Utils::get_last_day_of_month( strtotime( $the_query['start_date'] ) );
-		$the_query['end_date'] = substr_replace( $the_query['start_date'], $last_day, -2 );
-		$the_query['end_date'] = tribe_end_of_day( $the_query['end_date'] );
-
-		$query->query_vars = $the_query;
-	}
-
-	protected function next_upcoming_date() {
-		$next_event = tribe_get_events( array(
-			'eventDisplay'   => 'list',
-			'posts_per_page' => 1,
-			'start_date'     => date( 'Y-m-d' )
-		));
-
-		$start_date = date( 'Y-m' );
-
-		if ( ! empty( $next_event ) || isset( $next_event[0] ) ) {
-			$next_event_date = tribe_get_start_date( $next_event[0]->ID, false, 'Y-m' );
-
-			// Prevent calendar from rewinding to the start of a currently ongoing event
-			$start_date = ( $next_event_date > $start_date ) ? $next_event_date : $start_date;
+			return $start_date;
 		}
-
-		return $start_date;
 	}
 }
-
-// Brings the mini calendar to the next month with events.
-new Tribe_Advance_Minical();
